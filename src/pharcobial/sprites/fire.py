fbbbb85226ea372c240f0751aab7a2fd160e7506
unparalleled-js <yingthi@live.com>
2023-01-01 15:48:32 -0600

    feat: base fire sprite

---
+++ src/pharcobial/sprites/fire.py
@@ -0,0 +1,13 @@
+from pharcobial.sprites.base import NPC
+from pharcobial.types import Positional
+
+from pygame.transform import smoothscale
+
+
+class Fire(NPC):
+    def __init__(self, position: Positional, *args, **kwargs) -> None:
+        super().__init__("fire", position, "fire-1", (self.world.group, self.collision.group), (0, 0))
+        self.gfx_index = 0
+        self.gfx_total = 3
+        self.gfx_delay = 5
+        self.gfx_delay_index = 0
